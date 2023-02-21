d638ca9ae3cebef125c46e2353b0d61ba88879d6
Salov-it <salovit96@gmail.com>
2023-02-21 13:24:46 +0300

    Структура

---
+++ src/Ban.Domain/Ban.cs
@@ -0,0 +1,14 @@
+﻿
+namespace Ban.Domain
+{
+    public class Ban
+    {
+        public int id { get; set; }
+        public int entity { get; set; }
+        public string until { get; set; }
+        public string reason { get; set; }
+        public string createdAt { get; set; }
+        public string updatedAt { get; set; }
+
+    }
+}
